21d42cf2ac359db56e78c2916dca8af7dae89859
pablitodon <pavelkazakevich040196@gmail.com>
2025-03-14 19:44:11 +0300

    todo v1.03

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -49,14 +49,14 @@ const handleSubmit = () => {
     padding: 10px 20px;
     border: none;
     border-radius: 8px;
-    background-color: #00cec9; // Бирюзовый цвет
+    background-color: #0c4e4d;
     color: white;
     font-size: 16px;
     cursor: pointer;
     transition: background-color 0.3s ease, transform 0.2s ease;
 
     &:hover {
-      background-color: #00b4b1;
+      background-color: #032c2b;
       transform: translateY(-2px);
     }
 
